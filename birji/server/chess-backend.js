0f736565691c0290d0d5ea38ba4de46300b640c2
uzbek5000 <cytroniks.games@gmail.com>
2026-02-12 13:54:59 +0100

    /move v1 done

--- birji/server/chess-backend.js
+++ birji/server/chess-backend.js
@@ -4,7 +4,8 @@ import crypto from 'crypto';
 let games = new Map();
 
 function finishGame(gameId){
-    //TODO: end game, save to db
+    //TODO: save to db
+    games.delete(gameId);
 }
 
 export function registerChessRoutes(app) {
@@ -22,11 +23,34 @@ export function registerChessRoutes(app) {
 
         games.set(game.id, game);
 
-        return res.status(200).send(game);
+        return res.status(200).json({game: game});
     })
 
     app.post('/api/chess/move', (req, res) => {
-        const { gameId, from, to } = req.body;
-        //TODO: attempt move, apply move, error handling
+        const { gameId, from, to, promotion } = req.body;
+
+        const game = games.get(gameId);
+        if(!game){
+            return res.status(404).json({ error: "Game not found" });
+        }
+
+        const chess = game.chess;
+
+        const move = chess.move({
+            from: from,
+            to: to,
+            promotion: promotion || 'q'
+        });
+        if(!move) {
+            return res.status(400).json({ error: "Illegal move" });
+        }
+
+
+        const gameOver = chess.isGameOver();
+        if(gameOver){
+            finishGame(gameId);
+        }
+
+        return res.status(200).json({ id: gameId, fen: chess.fen(), pgn: chess.pgn(), status: gameOver });
     });
 }
